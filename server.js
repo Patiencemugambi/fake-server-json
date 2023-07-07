68ec37f6e56a01eac516b3cd989ccbde939efb57
Patiencemugambi <patiencemugambi@gmail.com>
2023-07-08 01:12:00 +0300

    database

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router("db.json"); // <== Will be created later
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3200;
+const port = process.env.PORT || 3200; // <== You can change the port
 
 server.use(middlewares);
 server.use(router);
 
-console.log(`Starting server.....`)
 server.listen(port);
-console.log(`Server running in port ${port}`)
